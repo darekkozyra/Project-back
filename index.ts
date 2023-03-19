0db8505cf5c3f7758a98dc65cec8475caf97260c
Darek Kozyra <darosg1@gmail.com>
2023-03-19 16:48:51 +0100

    Error handling & dabase config

--- index.ts
+++ index.ts
@@ -1,13 +1,19 @@
-import express from 'express';
+import express, {json} from 'express';
 import cors from 'cors';
 import 'express-async-errors';
+import {handleError, ValidationError} from "./utils/error";
 
 const app = express();
-
 app.use(cors({
-    origin: 'http://localhost:3000'
+    origin: 'http://localhost:3000',
 }));
 app.use(express.json());
+app.use(json());
+app.use(handleError)
+
+// app.get('/', async (req, res)=>{
+//      throw new ValidationError('Bład');
+// })
 
 app.listen(3001, 'localhost', ()=>{
     console.log('Listening on port http://localhost:3001');
